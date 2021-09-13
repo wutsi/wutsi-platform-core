d013583bf00f807cf6523a5ac89244486f84d2f2
htchepannou <htchepannou@expedia.com>
2021-09-13 18:06:01 -0400

    remove excessive logging

--- src/main/kotlin/com/wutsi/platform/core/security/spring/jwt/JWTAuthenticationProvider.kt
+++ src/main/kotlin/com/wutsi/platform/core/security/spring/jwt/JWTAuthenticationProvider.kt
@@ -1,16 +1,10 @@
 package com.wutsi.platform.core.security.spring.jwt
 
-import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.core.Authentication
 
 class JWTAuthenticationProvider : AuthenticationProvider {
-    companion object {
-        private val LOGGER = LoggerFactory.getLogger(JWTAuthenticationProvider::class.java)
-    }
-
     override fun authenticate(auth: Authentication): Authentication {
-        LOGGER.debug("Authenticating $auth")
         auth.isAuthenticated = true
         return auth
     }
