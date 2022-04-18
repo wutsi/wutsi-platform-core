6d7bfba132a354448e113647fb8fd6f53b458fdb
wutsi <herve.tchepannou@wutsi.com>
2022-04-18 08:59:34 -0400

    update logs

--- src/main/kotlin/com/wutsi/platform/core/image/spring/ImageConfigurationImageKit.kt
+++ src/main/kotlin/com/wutsi/platform/core/image/spring/ImageConfigurationImageKit.kt
@@ -2,6 +2,7 @@ package com.wutsi.platform.core.image.spring
 
 import com.wutsi.platform.core.image.ImageService
 import com.wutsi.platform.core.image.imagekit.ImageKitService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
@@ -16,7 +17,13 @@ open class ImageConfigurationImageKit(
     @Value("\${wutsi.platform.image.image-kit.origin-url}") private val originUrl: String,
     @Value("\${wutsi.platform.image.image-kit.endpoint-url}") private val endpointUrl: String
 ) {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(ImageConfigurationImageKit::class.java)
+    }
+
     @Bean
-    open fun imageService(): ImageService =
-        ImageKitService(originUrl, endpointUrl)
+    open fun imageService(): ImageService {
+        LOGGER.info("Creating ImageService")
+        return ImageKitService(originUrl, endpointUrl)
+    }
 }
--- src/main/kotlin/com/wutsi/platform/core/image/spring/ImageConfigurationNone.kt
+++ src/main/kotlin/com/wutsi/platform/core/image/spring/ImageConfigurationNone.kt
@@ -2,6 +2,7 @@ package com.wutsi.platform.core.image.spring
 
 import com.wutsi.platform.core.image.ImageService
 import com.wutsi.platform.core.image.none.ImageServiceNone
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,7 +14,13 @@ import org.springframework.context.annotation.Configuration
     matchIfMissing = true
 )
 open class ImageConfigurationNone {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(ImageConfigurationNone::class.java)
+    }
+
     @Bean
-    open fun imageService(): ImageService =
-        ImageServiceNone()
+    open fun imageService(): ImageService {
+        LOGGER.info("Creating ImageService")
+        return ImageServiceNone()
+    }
 }
