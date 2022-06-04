34bbeba6049e525cb79a5669490ac41a3d5e2037
wutsi <herve.tchepannou@wutsi.com>
2022-06-04 07:47:17 -0400

    fix resolution of token

--- src/main/kotlin/com/wutsi/platform/core/security/spring/DynamicTokenProvider.kt
+++ src/main/kotlin/com/wutsi/platform/core/security/spring/DynamicTokenProvider.kt
@@ -1,7 +1,6 @@
 package com.wutsi.platform.core.security.spring
 
 import com.wutsi.platform.core.security.TokenProvider
-import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationContext
 import org.springframework.web.context.request.RequestContextHolder
 
@@ -9,23 +8,12 @@ class DynamicTokenProvider(
     private val context: ApplicationContext,
     private val applicationTokenProvider: ApplicationTokenProvider
 ) : TokenProvider {
-    companion object {
-        private val LOGGER = LoggerFactory.getLogger(DynamicTokenProvider::class.java)
-    }
-
-    override fun getToken(): String? {
-        val delegate = get()
-        if (delegate == null) {
-            LOGGER.debug("No TokenProvider available in Application Context")
-            return null
-        } else {
-            return delegate.getToken()
-        }
-    }
+    override fun getToken(): String? =
+        get()?.getToken() ?: applicationTokenProvider.getToken()
 
     private fun get(): TokenProvider? =
         if (RequestContextHolder.getRequestAttributes() != null)
             context.getBean(RequestTokenProvider::class.java)
         else
-            applicationTokenProvider
+            null
 }
